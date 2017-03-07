21bfc40b956942fbf05b6598fdbd8d40ffde5466
umfc <lospok2@yandex.ru>
2017-03-07 17:38:51 +0300

    Update Form1.Designer.cs

--- ScreenshotUploaderGUI/Form1.Designer.cs
+++ ScreenshotUploaderGUI/Form1.Designer.cs
@@ -2,15 +2,7 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Обязательная переменная конструктора.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,11 +13,6 @@
         }
 
         #region Код, автоматически созданный конструктором форм Windows
-
-        /// <summary>
-        /// Требуемый метод для поддержки конструктора — не изменяйте 
-        /// содержимое этого метода с помощью редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
@@ -47,22 +34,13 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // openFileDialog1
-            // 
             this.openFileDialog1.Filter = "Images|*.bmp;*.jpg;*.gif;*.png;*.jpeg";
-            // 
-            // appid
-            // 
             this.appid.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.appid.ForeColor = System.Drawing.Color.White;
             this.appid.Location = new System.Drawing.Point(32, 100);
             this.appid.Name = "appid";
             this.appid.Size = new System.Drawing.Size(147, 20);
             this.appid.TabIndex = 1;
-            // 
-            // label2
-            // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -72,27 +50,18 @@
             this.label2.Size = new System.Drawing.Size(40, 15);
             this.label2.TabIndex = 2;
             this.label2.Text = "AppID";
-            // 
-            // location
-            // 
             this.location.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.location.ForeColor = System.Drawing.Color.White;
             this.location.Location = new System.Drawing.Point(197, 100);
             this.location.Name = "location";
             this.location.Size = new System.Drawing.Size(228, 20);
             this.location.TabIndex = 3;
-            // 
-            // path
-            // 
             this.path.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.path.ForeColor = System.Drawing.Color.White;
             this.path.Location = new System.Drawing.Point(32, 150);
             this.path.Name = "path";
             this.path.Size = new System.Drawing.Size(312, 20);
             this.path.TabIndex = 4;
-            // 
-            // steamids
-            // 
             this.steamids.AccessibleDescription = "";
             this.steamids.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.steamids.ForeColor = System.Drawing.Color.White;
@@ -102,9 +71,6 @@
             this.steamids.Size = new System.Drawing.Size(169, 47);
             this.steamids.TabIndex = 5;
             this.steamids.Text = "One SteamID64 per line";
-            // 
-            // label3
-            // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -114,9 +80,6 @@
             this.label3.Size = new System.Drawing.Size(56, 15);
             this.label3.TabIndex = 6;
             this.label3.Text = "Location";
-            // 
-            // label4
-            // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -126,9 +89,6 @@
             this.label4.Size = new System.Drawing.Size(71, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Image Path";
-            // 
-            // label5
-            // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -138,9 +98,6 @@
             this.label5.Size = new System.Drawing.Size(94, 15);
             this.label5.TabIndex = 8;
             this.label5.Text = "Tagged Players";
-            // 
-            // browse
-            // 
             this.browse.BackColor = System.Drawing.Color.Transparent;
             this.browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.browse.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -152,9 +109,6 @@
             this.browse.Text = "Browse";
             this.browse.UseVisualStyleBackColor = false;
             this.browse.Click += new System.EventHandler(this.browse_Click);
-            // 
-            // start
-            // 
             this.start.BackColor = System.Drawing.Color.Transparent;
             this.start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.start.Font = new System.Drawing.Font("Arial", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
@@ -166,9 +120,6 @@
             this.start.Text = "Upload";
             this.start.UseVisualStyleBackColor = false;
             this.start.Click += new System.EventHandler(this.start_Click);
-            // 
-            // label7
-            // 
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -179,9 +130,6 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "Made by Umfc";
             this.label7.Click += new System.EventHandler(this.label7_Click);
-            // 
-            // checkTagged
-            // 
             this.checkTagged.AutoSize = true;
             this.checkTagged.BackColor = System.Drawing.Color.Transparent;
             this.checkTagged.Location = new System.Drawing.Point(540, 83);
@@ -190,9 +138,6 @@
             this.checkTagged.TabIndex = 13;
             this.checkTagged.UseVisualStyleBackColor = false;
             this.checkTagged.CheckStateChanged += new System.EventHandler(this.checkTagged_CheckStateChanged);
-            // 
-            // output
-            // 
             this.output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.output.ForeColor = System.Drawing.Color.White;
             this.output.Location = new System.Drawing.Point(491, 153);
@@ -200,9 +145,6 @@
             this.output.ReadOnly = true;
             this.output.Size = new System.Drawing.Size(251, 20);
             this.output.TabIndex = 14;
-            // 
-            // label8
-            // 
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold);
@@ -212,9 +154,6 @@
             this.label8.Size = new System.Drawing.Size(45, 15);
             this.label8.TabIndex = 15;
             this.label8.Text = "Output";
-            // 
-            // pictureBox1
-            // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.Location = new System.Drawing.Point(106, -29);
@@ -222,9 +161,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(558, 100);
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
-            // 
-            // Form1
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
